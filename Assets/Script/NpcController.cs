a1cfc9da76f8442915c80612292ba9f50761bc76
Moussa <Moussa.Keita2411@gmail.com>
2021-04-02 22:17:57 -0400

    refactoring

---
+++ Assets/Script/NpcController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    private GameObject _target;
+    private NavMeshAgent _agent;
+     
+    void Start()
+    {
+        //Peut la mettre public pour permettre de mettre un GO qu'il devra suivre
+        _target = GameObject.FindGameObjectWithTag("Player");
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        detection();
+    }
+
+    private void detection()
+    {
+        float distance = Vector3.Distance(_target.transform.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            _agent.isStopped = false;
+            _agent.SetDestination(_target.transform.position);
+        }
+
+        if (distance >= lookRadius + 5)
+        {
+            _agent.isStopped = true;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,lookRadius);
+    }
+}
