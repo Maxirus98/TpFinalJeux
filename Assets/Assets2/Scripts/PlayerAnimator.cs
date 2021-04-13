78051d0241ce797f25bc07714b437dcdff123afe
Moussa <Moussa.Keita2411@gmail.com>
2021-04-13 09:04:53 -0400

    fixing conflicts from merge

--- Assets/Assets2/Scripts/PlayerAnimator.cs
+++ Assets/Assets2/Scripts/PlayerAnimator.cs
@@ -3,48 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class PlayerAnimator : MonoBehaviour
 {
     private const float LOCOMOTION_SMOOTH_TIME = .1f;
     private Animator _animator;
     private NavMeshAgent _agent;
-<<<<<<< HEAD
-
-
-    public bool _isAttacking = false;
-    private float _attackNumber;
-=======
     [SerializeField] private GameObject nuage;
-
+    [SerializeField] private GameObject rifle;
     public bool _isAttacking = false;
     private float _attackNumber;
     
->>>>>>> fbaa4618d9c7ae8ab95131befc738f521c7f3490
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponentInChildren<Animator>();
     }
-
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-=======
         if (Input.GetButton("Jump"))
         {
             CallCriDuTonnerre();
         }
->>>>>>> fbaa4618d9c7ae8ab95131befc738f521c7f3490
         //agent current speed / agent maximum speed
         float speedPercent = _agent.velocity.magnitude / _agent.speed * 10;
         //Will take 0.1 sec to change animation
         _animator.SetFloat("speedPercent", speedPercent, LOCOMOTION_SMOOTH_TIME, Time.deltaTime);
         
     }
-
     public void Attack()
     {
         _isAttacking = true;
@@ -54,24 +41,18 @@ public class PlayerAnimator : MonoBehaviour
         //StartCoroutine(CoroutineAttack());
         
     }
-<<<<<<< HEAD
-    
-=======
-
     private void CallCriDuTonnerre()
     {
         var cloneNuage = Instantiate(nuage, transform.position,
-            Quaternion.Euler(90, 0, 0));
+            Quaternion.AngleAxis(90, new Vector3(1, 0, 0)));
         Destroy(cloneNuage, 1f);
     }    
->>>>>>> fbaa4618d9c7ae8ab95131befc738f521c7f3490
     public IEnumerator CoroutineAttack()
     {
         yield return new WaitForSeconds(0.5f);
         _isAttacking = false;
         _animator.SetBool("isAttacking", _isAttacking);
     }
-
     void SetAttackNumber()
     {
         if (_attackNumber < 2)
@@ -83,5 +64,4 @@ public class PlayerAnimator : MonoBehaviour
             _attackNumber = 0;
         }
     }
-
 }
