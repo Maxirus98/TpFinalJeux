5d32f7081c3cff302fb1fc4ce925103985bf44c8
Moussa <Moussa.Keita2411@gmail.com>
2021-04-15 17:25:20 -0400

    adding an inventoryManager as a Singleton

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    #region Singleton
+    private static InventoryManager _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            print("single instance of inventory");
+            _instance = this;
+        }
+    }
+    #endregion
+
+    public List<Item> items = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+        if(!item.isDefaultItem)
+            items.Add(item);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+    }
+}
