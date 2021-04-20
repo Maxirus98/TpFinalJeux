32f61b73a403119ed5debff713c28b58dbf775af
Moussa <Moussa.Keita2411@gmail.com>
2021-04-20 14:35:49 -0400

    adding an onclickEvent to be able to remove item from slot

--- Assets/Scripts/InventoryScripts/InventorySlotUI.cs
+++ Assets/Scripts/InventoryScripts/InventorySlotUI.cs
@@ -1,16 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class InventorySlotUI : MonoBehaviour
 {
     public Item _item;
     public Image _icon;
+    public Button removeButton;
+
+     void Awake()
+     {
+         removeButton = GetComponentsInChildren<Button>()[1];
+     }
 
     public void PutItemInSlot(Item item)
     {
         _item = item;
         _icon.sprite = item.icon;
         _icon.enabled = true;
+        removeButton.interactable = true;
         print("put in slot");
     }
 
@@ -19,5 +27,11 @@ public class InventorySlotUI : MonoBehaviour
         _item = null;
         _icon.sprite = null;
         _icon.enabled = false;
+        removeButton.interactable = false;
+    }
+
+    public void OnClickRemoveButton()
+    {
+        InventoryManager._instance.RemoveItem(_item);
     }
 }
