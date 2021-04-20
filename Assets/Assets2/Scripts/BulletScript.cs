6fcd42841d92697da01e1e6548720808d20b9e08
Maxirus98 <dupuismaxime@hotmail.com>
2021-04-20 09:59:08 -0400

    On enlève deux personnages qui ne rendent pas le jeu plus amusant : Puttin et Zola, car leur habileté n’ajoute rien au jeu.

--- Assets/Assets2/Scripts/BulletScript.cs
+++ Assets/Assets2/Scripts/BulletScript.cs
@@ -7,7 +7,7 @@ public class BulletScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Enemy"))
         {
             Destroy(gameObject);
         }
