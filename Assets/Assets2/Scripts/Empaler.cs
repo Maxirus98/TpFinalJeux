b27080f0e4f7d5d10c546945b37f58db0161133f
Maxirus98 <dupuismaxime@hotmail.com>
2021-04-27 08:35:16 -0400

    refactoring du spell empaler, velocity au lieu de AddForce et Nouvelle classe enfant Empaler qui extends Spell

---
+++ Assets/Assets2/Scripts/Empaler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class Empaler : Spell
+{
+    private float vitesseEmpaler = 1000f;
+    private GameObject empalerClone;
+    void Update()
+    {
+        position = transform.position;
+        if (Input.GetButton("Jump") && Time.time >= TimeStamp)
+        {
+            DoSpell();
+        }
+    }
+
+    public override void DoSpell()
+    {
+        Spawn();
+        Shoot();
+        StartCoroutine(Cooldown.WaitFor(cooldown));
+        TimeStamp = Time.time + cooldown;
+    }
+
+    public override void Spawn()
+    {
+        empalerClone = Instantiate(go, position, transform.rotation);
+        Destroy(empalerClone, 0.5f);
+    }
+
+    public override void Shoot()
+    {
+        empalerClone.GetComponent<Rigidbody>().velocity = (transform.forward * (vitesseEmpaler * Time.deltaTime));;
+    }
+}
