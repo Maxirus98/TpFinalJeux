58abd622a58f5bcd6a826391a928eaf0e4c74e9a
Moussa <Moussa.Keita2411@gmail.com>
2021-04-29 15:16:45 -0400

    adding a timer.cs

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 600;
+    private TextMeshPro _textMeshPro;
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            time = 0;
+        }
+    }
+
+    void DisplayTime(float time)
+    {
+        
+    }
+}
