dfc90e9598466ecca53039151cc9964ddb9e8ff9
Moussa <Moussa.Keita2411@gmail.com>
2021-04-30 23:36:07 -0400

    refactoring LevelTrackerService

--- Assets/Scripts/Blurry.cs
+++ Assets/Scripts/Blurry.cs
@@ -8,42 +8,27 @@ using UnityEngine.UI;
 
 public class Blurry : MonoBehaviour
 {
+    public Material material;
     private Image _image;
+    private Button _button;
     private TextMeshProUGUI _textMeshPro;
     private string _dimensionName;
 
     void Start()
     {
         _image = GetComponentInChildren<Image>();
+        _button = GetComponentInChildren<Button>();
         _textMeshPro = GetComponentInChildren<TextMeshProUGUI>();
         Blurriness();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     void Blurriness()
     {
-        _dimensionName = _textMeshPro.text;
-        string[] dimensions = LevelTrackerService.Get().Split(',');
-        string keyPair = GetKeyValuePair(dimensions);
-        print(_textMeshPro.name + _image.material.name);
-        // print(LevelTrackerService.Get().Split(',')[0]);
+        _dimensionName = _textMeshPro.text.ToLower();
+        string dimensions = LevelTrackerService.GetOne(_dimensionName);
+        
+           // _button.gameObject.SetActive(true);
+           // _image.material = null;
     }
 
-    public void Clear()
-    {
-    }
-
-    private string GetKeyValuePair(string[] pairs)
-    {
-        foreach (var pair in pairs)
-        {
-            if (pair.Split(':')[0].Equals(_dimensionName))
-                return pair;
-        }
-        return String.Empty;
-    }
 }
--- Assets/Scripts/Services/LevelTrackerService.cs
+++ Assets/Scripts/Services/LevelTrackerService.cs
@@ -6,14 +6,14 @@ namespace Services
 {
     public abstract class LevelTrackerService
     {
-        public static string Get()
+        //Question Prof est-ce une bonne utilisation du static?
+        public static string GetAll()
         {
             try
             {
-               string path = Application.dataPath + "/Resources/LevelTracking.txt";
-               string data = File.ReadAllText(path);
-               return data ;
-
+                string path = Application.dataPath + "/Resources/LevelTracking.txt";
+                string data = File.ReadAllText(path);
+                return data;
             }
             catch (Exception e)
             {
@@ -22,7 +22,24 @@ namespace Services
 
             return null;
         }
-        
-        
+
+        public static string GetOne(string name)
+        {
+            string[] levels = GetAll().Split(',');
+
+            foreach (var level in levels)
+            {
+                string dimensionName = level.Split(':')[0].ToLower().Trim();
+                string status = level.Split(':')[1].ToLower().Trim();
+
+                if (name.Trim().Equals(dimensionName) && status.Equals("unlocked"))
+                {
+                    Debug.Log(dimensionName + " " + status);
+                    return level;
+                }
+            }
+
+            return String.Empty;
+        }
     }
 }
