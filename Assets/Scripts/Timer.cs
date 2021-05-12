d8e9fdb700c09a699d73637e731da6cf9acd1cc7
Moussa <Moussa.Keita2411@gmail.com>
2021-05-11 22:46:39 -0400

    adding soundClip for timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,10 +6,12 @@ public class Timer : MonoBehaviour
 {
     public float timeLeft = 600;
     private TextMeshProUGUI _textMeshPro;
+    private AudioSource _audioSource;
 
     void Awake()
     {
         _textMeshPro = GetComponentInChildren<TextMeshProUGUI>();
+        _audioSource = GetComponent<AudioSource>();
         print(_textMeshPro.name);
     }
 
@@ -19,12 +21,20 @@ public class Timer : MonoBehaviour
         {
             timeLeft -= Time.deltaTime;
         }
+
+        else if(timeLeft == 30)
+        {
+          _audioSource.Play();  
+        }
+        
         else
         {
             timeLeft = 0;
+            _audioSource.Stop();
             GameManager.Instance.LoadLevel("GameOver");
         }
-
+        
+        _audioSource.loop = timeLeft <= 30 && timeLeft >= 0;
         DisplayTime(timeLeft);
     }
 
@@ -34,10 +44,19 @@ public class Timer : MonoBehaviour
         {
             time = 0;
         }
+
         float minutes = Mathf.FloorToInt(time / 60);
         float seconds = Mathf.FloorToInt(time % 60);
         if (minutes < 1)
+        {
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
+
             _textMeshPro.color = Color.red;
+        }
+
         _textMeshPro.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
