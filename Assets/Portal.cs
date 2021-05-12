425297991d6a517e89f2fefda7579460a42a237d
Moussa <Moussa.Keita2411@gmail.com>
2021-05-12 00:34:38 -0400

    commit

---
+++ Assets/Portal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private ParticleSystem _particleSystem;
+    private Transform _playerTransform;
+
+    void Start()
+    {
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _particleSystem = GetComponent<ParticleSystem>();
+        _particleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Boss") == null && !_particleSystem.isPlaying)
+        {
+            StartCoroutine(OnVoidAppear());
+        }
+    }
+
+    public IEnumerator OnVoidAppear()
+    {
+        transform.position = _playerTransform.position;
+        transform.LookAt(_playerTransform);
+        _particleSystem.Play();
+        yield return new WaitForSeconds(5f);
+        GameManager.Instance.LoadLevel("SelectLevel");
+    }
+}
